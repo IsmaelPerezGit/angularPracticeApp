3ac0157a71eedd412b7faf6ba21345224a70b0df
Ismael Perez <ismaelperezemail@gmail.com>
2018-08-21 17:32:03 -0700

    App now utilizes api

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from '../data.service';
+import {Observable} from 'rxjs';
+import {Data} from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  users$: Object;
+
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getUsers().subscribe(
+      data => this.users$ = data
+    );
   }
-
 }
